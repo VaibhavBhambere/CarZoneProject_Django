22b7ffed5194fd24188c788669090fc1910caa87
Vaibhav Bhambere <vbhambere5@gmail.com>
2023-12-19 15:58:36 +0530

    half project

--- carzone/pages/views.py
+++ carzone/pages/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 
+from pages.models import Team
+
 # Create your views here.
 def home(request):
-    return render(request,'pages/home.html')
+    team=Team.objects.all()
+    context={
+        'team': team,
+    }
+    return render(request,'pages/home.html',context)
 
 def about(request):
-    return render(request,'pages/about.html')
+    team=Team.objects.all()
+    context={
+        'team': team,
+    }
+    return render(request,'pages/about.html',context)
 
 def contact(request):
     return render(request,'pages/contact.html')
